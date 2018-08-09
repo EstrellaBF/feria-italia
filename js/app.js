ac699d1e6c1ab0bc1953a43adc96cc47def83b3b
EstrellaBF <stern.von.nord@gmail.com>
2018-08-08 23:54:58 -0500

    falta mejorar codigo

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 //window.addEventListener('load', function() {});
 
 // create the module and name it demoApp
-var italiaApp = angular.module('italiaApp', ['ngRoute']);
+//var italiaApp = angular.module('italiaApp', ['ngRoute']);
 /*
 // create the controller and inject Angular's $scope
 italiaApp.controller('mainCtrl', function ($scope) {
@@ -37,63 +37,79 @@ let selectOption = function () {
 // Selected option on modal
 $('.dropdown-item').on('click', selectOption);
 
-
-  var sponsorsImg = [
-    { src: "assets/logos/apavit.png" },
-    { src: "assets/logos/cdo.png" },
-    { src: "assets/logos/circolo.png" },
-    { src: "assets/logos/comites.png" },
-    { src: "assets/logos/ccl.png" },
-    { src: "assets/logos/lucani.png" },
-    { src: "assets/logos/opere.png" },
-    { src: "assets/logos/piazza-italia.png" },
-    { src: "assets/logos/ucss.png" }
-  ];
-
-  var sponsorsIcons = document.getElementById("sponsors-icons");
-  var sponsorsDiv = document.getElementById("sponsors-div");
-  /*
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  
-  
-  var slideIndex = 1;
-  showSlides(slideIndex);
-  
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
+/*
+var sponsorsImg = [
+  { src: "assets/logos/apavit.png" },
+  { src: "assets/logos/cdo.png" },
+  { src: "assets/logos/circolo.png" },
+  { src: "assets/logos/comites.png" },
+  { src: "assets/logos/ccl.png" },
+  { src: "assets/logos/lucani.png" },
+  { src: "assets/logos/opere.png" },
+  { src: "assets/logos/piazza-italia.png" },
+  { src: "assets/logos/ucss.png" }
+];
+*/
+var sponsorsIcons = document.getElementById("sponsors-icons");
+var sponsorsDiv = document.getElementById("sponsors-div");
+/*
+var slides = document.getElementsByClassName("mySlides");
+var dots = document.getElementsByClassName("dot");
+ 
+ 
+var slideIndex = 1;
+showSlides(slideIndex);
+ 
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+ 
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+ 
+function showSlides() {
+  var i;
+  for (i = 0; i < slides.length; i++) {
+     slides[i].style.display = "none";  
   }
-  
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
   }
-  
-  function showSlides() {
-    var i;
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 5000); // Change image every 2 seconds
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+*/
+
+//section sponsors
+//var img = document.createElement("IMG");
+//sponsorsDiv.appendChild(img)
+
+/*
+for (var i = 0; i < sponsorsImg.length; i++) {
+
+  var img = document.createElement("IMG");
+  sponsorsIcons.appendChild(img);
+  img.setAttribute("src", sponsorsImg[i].src);
+  //console.log("posicion " + i + ":" + img[i]);
+}
+*/
+
+$('.carousel').carousel()
+
+window.onscroll = function () {
+  //var scroll = window.scrollTop();
+  var scroll = document.documentElement.scrollTop || document.body.scrollTop;
+  console.log(scroll);
+  if (scroll > 640) {
+    console.log('supera a 630');
+    //$menuHome.css('visibility', 'visible');
+    // $menuHome.addClass('menu-home');
+  } else {
+    console.log('no supera');
+    // $menuHome.hide();
   }
-  */
-
-  //section sponsors
-  //var img = document.createElement("IMG");
-  //sponsorsDiv.appendChild(img)
-
-  for (var i = 0; i < sponsorsImg.length; i++) {
-
-    var img = document.createElement("IMG");
-    sponsorsIcons.appendChild(img);
-    img.setAttribute("src", sponsorsImg[i].src);
-    //console.log("posicion " + i + ":" + img[i]);
-  }
-
-  $('.carousel').carousel()
+};
