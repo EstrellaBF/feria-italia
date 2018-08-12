6aa2467ed54d291f1cb46f017cf4fb3f18ff7c16
EstrellaBF <stern.von.nord@gmail.com>
2018-08-11 20:09:48 -0500

    modifying structure

--- js/app.js
+++ js/app.js
@@ -2,31 +2,6 @@
 
 //window.addEventListener('load', function() {});
 
-// create the module and name it demoApp
-//var italiaApp = angular.module('italiaApp', ['ngRoute']);
-/*
-// create the controller and inject Angular's $scope
-italiaApp.controller('mainCtrl', function ($scope) {
-  $scope.message = 'Hello world!';
-});
-
-italiaApp.config(function($routeProvider, $locationProvider) {
-  $routeProvider
-
-    .when('/restaurant' , {
-      templateUrl : 'restaurant.html',
-      controller  : 'mainCtrl2'
-    });
-});
-
-    // create the controller and inject Angular's $scope
-    italiaApp.controller('mainCtrl2', function($scope) {
-      // create a message to display in our view
-      $scope.message = 'Hello world, this is the home page!';
-  });
-
-*/
-
 /*botones */
 let selectOption = function () {
   var selected = $(this).text();
@@ -34,69 +9,10 @@ let selectOption = function () {
   $(this).parent().parent().parent().find('button').html(selected + '&nbsp;<span class="caret"></span>');
 }
 
-// Selected option on modal
-$('.dropdown-item').on('click', selectOption);
 
-/*
-var sponsorsImg = [
-  { src: "assets/logos/apavit.png" },
-  { src: "assets/logos/cdo.png" },
-  { src: "assets/logos/circolo.png" },
-  { src: "assets/logos/comites.png" },
-  { src: "assets/logos/ccl.png" },
-  { src: "assets/logos/lucani.png" },
-  { src: "assets/logos/opere.png" },
-  { src: "assets/logos/piazza-italia.png" },
-  { src: "assets/logos/ucss.png" }
-];
-*/
 var sponsorsIcons = document.getElementById("sponsors-icons");
 var sponsorsDiv = document.getElementById("sponsors-div");
-/*
-var slides = document.getElementsByClassName("mySlides");
-var dots = document.getElementsByClassName("dot");
- 
- 
-var slideIndex = 1;
-showSlides(slideIndex);
- 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
- 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
- 
-function showSlides() {
-  var i;
-  for (i = 0; i < slides.length; i++) {
-     slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
-}
-*/
 
-//section sponsors
-//var img = document.createElement("IMG");
-//sponsorsDiv.appendChild(img)
-
-/*
-for (var i = 0; i < sponsorsImg.length; i++) {
-
-  var img = document.createElement("IMG");
-  sponsorsIcons.appendChild(img);
-  img.setAttribute("src", sponsorsImg[i].src);
-  //console.log("posicion " + i + ":" + img[i]);
-}
-*/
 
 $('.carousel').carousel()
 
@@ -113,3 +29,48 @@ window.onscroll = function () {
     // $menuHome.hide();
   }
 };
+
+
+ //Fechas inicial y final
+ /*
+ var d = new Date();
+console.log(d.getMonth() +1);
+
+ document.getElementById("fecha").append(`${ - (d.getMonth() +1)}`)
+ */
+
+var end = new Date('08/13/2018 9:30 AM');
+
+var _second = 1000;
+var _minute = _second * 60;
+var _hour = _minute * 60;
+var _day = _hour * 24;
+var timer;
+
+//function showRemaining() {
+    var now = new Date();
+    var distance = end - now;
+    /*
+    if (distance < 0) {
+
+        clearInterval(timer);
+        document.getElementById('countdown').innerHTML = 'EXPIRED!';
+
+        return;
+    }
+    */
+    var days = Math.floor(distance / _day);
+    var hours = Math.floor((distance % _day) / _hour);
+    var minutes = Math.floor((distance % _hour) / _minute);
+    var seconds = Math.floor((distance % _minute) / _second);
+
+    console.log(days)
+    //document.getElementById('countdown-mobile').innerHTML = 'Faltan ' + days + ' dias, ';
+    //document.getElementById('countdown-mobile').innerHTML += hours + ' horas, ';
+    document.getElementById('countdown-desktop').innerHTML = 'Faltan ' + days + ' dias, ';
+    document.getElementById('countdown-desktop').innerHTML += hours + ' horas, ';
+    //document.getElementById('countdown').innerHTML += minutes + ' minutos y ';
+    //document.getElementById('countdown').innerHTML += seconds + ' segundos';
+//}
+
+//timer = setInterval(showRemaining);
